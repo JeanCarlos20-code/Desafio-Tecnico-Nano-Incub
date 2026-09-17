d9416d6dd55a560c472ddf106f544cec07f64af8
Jean Souza <jeancarlos272010@gmail.com>
2026-09-17 18:44:26 -0300

    feat(resources): post register form through users service

--- resources/js/Services/users.js
+++ resources/js/Services/users.js
@@ -1,3 +1,14 @@
-export function store(form) {
-    form.post('/users');
+export function store(form, options = {}) {
+    form.post('/register', {
+        onError: (errors) => {
+            form.reset('password');
+            options.onError?.(errors);
+        },
+        onHttpException: (response) => {
+            return options.onHttpException?.(response) ?? false;
+        },
+        onNetworkError: (error) => {
+            return options.onNetworkError?.(error) ?? false;
+        },
+    });
 }
