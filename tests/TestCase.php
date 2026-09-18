f3155a23f5692e1382fb985bee38f31d5cb3678f
Jean Souza <jeancarlos272010@gmail.com>
2026-09-18 13:28:16 -0300

    test(tests): clear cached config before phpunit boot

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function createApplication(): Application
+    {
+        $cachedConfig = dirname(__DIR__).'/bootstrap/cache/config.php';
+
+        if (is_file($cachedConfig)) {
+            unlink($cachedConfig);
+        }
+
+        return parent::createApplication();
+    }
 }
